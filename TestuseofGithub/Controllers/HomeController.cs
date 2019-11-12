97cfde30cee4b44012aeabde5171f6d58bb4ee66
HAIDER IFTEKHAR ZAR <TP048606@mail.apu.edu.my>
2019-11-12 17:09:34 +0800

    change2

--- TestuseofGithub/Controllers/HomeController.cs
+++ TestuseofGithub/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace TestuseofGithub.Controllers
             return View();
         }
 
+        public IActionResult displayflower()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
